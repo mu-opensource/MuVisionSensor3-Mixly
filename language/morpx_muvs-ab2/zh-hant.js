2f2325e602235d1f465357a48524cbf6c1a7e499
俞世权 <sq.yu@morpx.com>
2019-09-03 13:53:46 +0800

    add language zh-hant

---
+++ language/morpx_muvs-ab2/zh-hant.js
@@ -0,0 +1,111 @@
+// This file was automatically generated. Do not modify. 
+'use strict'; 
+
+goog.provide('Blockly.Msg.zh.hans'); 
+
+goog.require('Blockly.Msg'); 
+
+
+/* Morpx Vision Sensor */ 
+/*Help文本*/ 
+Blockly.LKL_VS2_HELP_INIT = '初始化視覺傳感器，並選擇相關的接口'; 
+Blockly.LKL_VS2_HELP_SAVE = '保存視覺傳感器的相關設置（只能在設置模式中使用）'; 
+Blockly.LKL_VS2_HELP_VISION_LEVEL = '設置識別等級，等級升高，誤報率降低，識別率也會相應降低'; 
+Blockly.LKL_VS2_HELP_VISION_ZOOM = '設置圖像縮放等級，等級升高，識別距離越遠，識別角度越小'; 
+
+/*Warning文本**/ 
+Blockly.LKL_VS2_WARNING_SETUP_ONLY = '該模組只能置於設置模組中！'; 
+Blockly.LKL_VS2_WARNING_MU_INIT = '請勿同時使用Serial連接MU與電腦列印字符，此操作會導致電腦端列印字符錯亂或通信異常'; 
+
+/*參數文本*/ 
+//LED color type 
+Blockly.LKL_VS2_LED_CLOSE = '關閉'; 
+Blockly.LKL_VS2_LED_RED = '紅色'; 
+Blockly.LKL_VS2_LED_GREEN = '綠色'; 
+Blockly.LKL_VS2_LED_YELLOW = '黃色'; 
+Blockly.LKL_VS2_LED_BLUE = '藍色'; 
+Blockly.LKL_VS2_LED_PURPLE = '紫色'; 
+Blockly.LKL_VS2_LED_CYAN = '青色'; 
+Blockly.LKL_VS2_LED_WHITE = '白色'; 
+//Vision Zoom 
+Blockly.LKL_VS2_AUTO = '自動'; 
+Blockly.LKL_VS2_VISION_ZOOM = '縮放'; 
+//Vision Level 
+Blockly.LKL_VS2_LEVEL = '等級'; 
+Blockly.LKL_VS2_HIGH_SPEED = '速度優先'; 
+Blockly.LKL_VS2_NORMAL = '性能均衡'; 
+Blockly.LKL_VS2_HIGH_ACCURACY = '准確率優先'; 
+//UART status 
+Blockly.LKL_VS2_UART_OPEN = '打開'; 
+Blockly.LKL_VS2_UART_CLOSE = '關閉'; 
+//Vision type 
+Blockly.LKL_VS2_COLOR_BLOCK = '色塊'; 
+Blockly.LKL_VS2_VISION_COLOR_DETECT = '色塊檢測'; 
+Blockly.LKL_VS2_VISION_COLOR_RECOGNITION = '顏色識別'; 
+Blockly.LKL_VS2_VISION_BALL = '球'; 
+Blockly.LKL_VS2_VISION_LINE = '線條'; 
+Blockly.LKL_VS2_VISION_BODY = '人體'; 
+Blockly.LKL_VS2_VISION_FACE = '人臉'; 
+Blockly.LKL_VS2_VISION_TRAFFIC_CARD = '交通卡片'; 
+Blockly.LKL_VS2_VISION_SHAPE_CARD = '形狀卡片'; 
+Blockly.LKL_VS2_VISION_NUM_CARD = '數字卡片'; 
+//Card type 
+Blockly.LKL_VS2_CARD_CIRCLE = '圓形'; 
+Blockly.LKL_VS2_CARD_TRIANGLE = '三角形'; 
+Blockly.LKL_VS2_CARD_SQUARE = '方形'; 
+Blockly.LKL_VS2_CARD_TICK = '鉤'; 
+Blockly.LKL_VS2_CARD_CROSS = '叉'; 
+Blockly.LKL_VS2_CARD_STRAIGHT = '向前'; 
+Blockly.LKL_VS2_CARD_TURN_LEFT = '向左'; 
+Blockly.LKL_VS2_CARD_TURN_RIGHT = '向右'; 
+Blockly.LKL_VS2_CARD_TURN_AROUND = '掉頭'; 
+Blockly.LKL_VS2_CARD_WHISTLE = '鳴笛'; 
+Blockly.LKL_VS2_CARD_STOP = '停止'; 
+//Vision state 
+Blockly.LKL_VS2_STATE_VALUE_X = '橫向坐標'; 
+Blockly.LKL_VS2_STATE_VALUE_Y = '縱向坐標'; 
+Blockly.LKL_VS2_STATE_VALUE_WIDTH = '寬度'; 
+Blockly.LKL_VS2_STATE_VALUE_HEIGHT = '高度'; 
+Blockly.LKL_VS2_STATE_VALUE_R_CHANNEL = '紅色通道'; 
+Blockly.LKL_VS2_STATE_VALUE_G_CHANNEL = '綠色通道'; 
+Blockly.LKL_VS2_STATE_VALUE_B_CHANNEL = '藍色通道'; 
+Blockly.LKL_VS2_STATE_VALUE_LABEL = '標籤'; 
+//True False 
+Blockly.LKL_VS2_TRUE = '是'; 
+Blockly.LKL_VS2_FALSE = '否'; 
+//Enable Disable 
+Blockly.LKL_VS2_ENABLE = '啟用'; 
+Blockly.LKL_VS2_DISABLE = '關閉'; 
+//Camera AWB 
+Blockly.LKL_VS2_LOCK_AWB = '鎖定白平衡'; 
+Blockly.LKL_VS2_WHITE_LIGHT = '白光模式'; 
+Blockly.LKL_VS2_YELLOW_LIGHT = '黃光模式'; 
+
+//Other 
+Blockly.LKL_VS2_COLOR = '顏色'; 
+
+/*模組文本*/ 
+Blockly.LKL_VS2_MU = '初始化'; 
+Blockly.LKL_VS2_MODE = '模式'; 
+Blockly.LKL_VS2_SetupVS = '設置'; 
+Blockly.LKL_VS2_SERIAL = '接口'; 
+Blockly.LKL_VS2_ADDRESS = '地址'; 
+Blockly.LKL_VS2_RESET = '恢復初始設置'; 
+Blockly.LKL_VS2_LED_DETECT_COLOR = '檢測到物體時顯示'; 
+Blockly.LKL_VS2_LED_UNDETECT_COLOR = '否則'; 
+Blockly.LKL_VS2_BRIGHTNESS = '亮度'; 
+Blockly.LKL_VS2_VISION_TYPE = '算法'; 
+Blockly.LKL_VS2_SET_VISION_LEVEL = '性能'; 
+Blockly.LKL_VS2_SET_FRAME_ROTATE = '翻轉圖像'; 
+Blockly.LKL_VS2_SET_CAMERA_HFR = '高幀率模式'; 
+Blockly.LKL_VS2_SET_CAMERA_AWB = '攝像頭白平衡'; 
+Blockly.LKL_VS2_SET_VISION_ZOOM = '數碼變焦'; 
+Blockly.LKL_VS2_SET_UART_BAUD = '串口鮑率'; 
+Blockly.LKL_VS2_DETECTED = '檢測到'; 
+Blockly.LKL_VS2_RECOGNIZED = '識別到'; 
+Blockly.LKL_VS2_GET_DETECTED_MESSAGE = '獲取'; 
+Blockly.LKL_VS2_VALUE = '值'; 
+Blockly.LKL_VS2_GET_PIX_COLOR = '捕獲坐標'; 
+Blockly.LKL_VS2_CARD_TYPE = '卡片類型'; 
+Blockly.LKL_VS2_SET_DETECT_COLOR = '設置識別顏色'; 
+Blockly.LKL_VS2_COORDINATE = '坐標';
